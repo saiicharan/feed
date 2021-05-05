7e4de383e24140671e4ccf65fe8da0ac0af08fc1
Sai Charan <charankuppa@gmail.com>
2021-05-05 16:27:01 +0200

    Add feed api

---
+++ index.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var app = express();
+
+var Feed = require('rss-to-json');
+
+const urls = ["https://www.reddit.com/r/sysadmin/.rss", "https://www.reddit.com/r/programming/.rss", "https://news.ycombinator.com/rss"];
+
+const getRandomFeedUrl = () => {
+    const randomUrlIndex = Math.floor(Math.random() * urls.length);
+    return urls[randomUrlIndex];
+}
+
+app.get("/feed", (req, res, next) => {
+    const url = getRandomFeedUrl();
+    Feed.load(url).then(response => {
+        res.json(response);
+    });
+});
+
+app.listen(process.env.PORT, () => {
+    console.log("Server running on port " + process.env.PORT);
+});
